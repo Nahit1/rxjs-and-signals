543b30c4868df8f9474832255ef002e68a4b5e5f
Nahit Ferhat Ektaş <nahit.ektas@acibadem.com>
2024-08-25 13:40:21 +0300

    .

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -14,49 +14,28 @@ export class MoviesService {
 
   page$ = new BehaviorSubject(1);
   loading$ = new BehaviorSubject(false);
-  movies$: Observable<CustomModel<Movie[]>>;
+  movies$ = this.page$.pipe(
+    tap(() => this.loading$.next(true)),
+    switchMap((page) => {
+      return this.httpClient.get<CustomModel<Movie[]>>(
+        this.baseUrl + 'discover/movie?page=' + page,
+        {
+          headers: {
+            Authorization: this.token,
+            accept: 'application/json',
+          },
+        }
+      );
+    }),
+    scan((acc, data) => {
+      return {
+        ...acc,
+        page: data.page,
+        results: [...acc.results, ...data.results],
+      };
+    }),
+    tap(() => this.loading$.next(false))
+  );
 
   private httpClient = inject(HttpClient);
-
-  constructor() {
-    this.movies$ = this.page$.pipe(
-      tap(() => this.loading$.next(true)),
-      switchMap((page) => {
-        return this.httpClient.get<CustomModel<Movie[]>>(
-          this.baseUrl + 'discover/movie?page=' + page,
-          {
-            headers: {
-              Authorization: this.token,
-              accept: 'application/json',
-            },
-          }
-        );
-      }),
-      scan((acc, data) => {
-        return {
-          ...acc,
-          page: data.page,
-          results: [...acc.results, ...data.results],
-        };
-      }),
-      tap(() => this.loading$.next(false))
-    );
-  }
-
-  // getMovies() {
-  //   return this.httpClient.get<CustomModel<Movie[]>>(
-  //     this.baseUrl + 'discover/movie',
-  //     {
-  //       headers: {
-  //         Authorization: this.token,
-  //         accept: 'application/json',
-  //       },
-  //     }
-  //   );
-  // }
 }
-
-// curl --request GET \
-//      --url 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc' \
-//      --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYWIyYWVkZDdkMDg2NGU3ODY1NTYyOTM5MmFlOGRhMyIsIm5iZiI6MTcyNDQ0NDM0Ni41OTIwOTYsInN1YiI6IjYzOWRjZTE1YmU2ZDg4MDA4NGViMTE1NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fef2QDjsJRxRFON1UGQUvpgktufOxlDE6oikqSJRnbk' \
-//      --header 'accept: application/json'
